9640447954744fa41df257a2f13a10241e3147af
jung No <jung@jungno.com>
2024-10-31 18:52:03 -0600

    3 user
    Signed-off-by: jung No <jung@jungno.com>

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -31,7 +31,7 @@ export default function RootLayout() {
     <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
       <CartProvider>
         <Stack>
-          <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+          <Stack.Screen name="(user)" options={{ headerShown: false }} />
           <Stack.Screen name="+not-found" />
           <Stack.Screen name="cart"
             options={{
