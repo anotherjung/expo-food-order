ddd81c09a9908bf05b1d2593ffceed0a8ee6c408
jung No <jung@jungno.com>
2024-11-15 17:39:21 -0600

    6 db
    Signed-off-by: jung No <jung@jungno.com>

---
+++ lib/supabase.ts
@@ -0,0 +1,27 @@
+import 'react-native-url-polyfill/auto';
+import * as SecureStore from 'expo-secure-store';
+import { createClient } from '@supabase/supabase-js';
+
+const ExpoSecureStoreAdapter = {
+  getItem: (key: string) => {
+    return SecureStore.getItemAsync(key);
+  },
+  setItem: (key: string, value: string) => {
+    SecureStore.setItemAsync(key, value);
+  },
+  removeItem: (key: string) => {
+    SecureStore.deleteItemAsync(key);
+  },
+};
+
+const supabaseUrl = 'https://mlhuqnftpthkvwvcwiqk.supabase.co';
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1saHVxbmZ0cHRoa3Z3dmN3aXFrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzE3MTM0OTUsImV4cCI6MjA0NzI4OTQ5NX0.KC1Mqau3jx7GS7kRNlxy4otTIZ6OkPFWwtAVhz878fY';
+
+export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
+  auth: {
+    storage: ExpoSecureStoreAdapter as any,
+    autoRefreshToken: true,
+    persistSession: true,
+    detectSessionInUrl: false,
+  },
+});
