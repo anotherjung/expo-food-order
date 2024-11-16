07253cf63a747e44def33f50d9d6e7597d735f63
jung No <jung@jungno.com>
2024-11-15 22:22:39 -0600

    8
    Signed-off-by: jung No <jung@jungno.com>

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,6 +1,7 @@
 import 'react-native-url-polyfill/auto';
 import * as SecureStore from 'expo-secure-store';
 import { createClient } from '@supabase/supabase-js';
+import { Database } from '@/database.types';
 
 const ExpoSecureStoreAdapter = {
   getItem: (key: string) => {
@@ -17,7 +18,7 @@ const ExpoSecureStoreAdapter = {
 const supabaseUrl = 'https://mlhuqnftpthkvwvcwiqk.supabase.co';
 const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1saHVxbmZ0cHRoa3Z3dmN3aXFrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzE3MTM0OTUsImV4cCI6MjA0NzI4OTQ5NX0.KC1Mqau3jx7GS7kRNlxy4otTIZ6OkPFWwtAVhz878fY';
 
-export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
+export const supabase = createClient<Database>(supabaseUrl, supabaseAnonKey, {
   auth: {
     storage: ExpoSecureStoreAdapter as any,
     autoRefreshToken: true,
